0782715b268fae8f785d2e834ea04dcc71ec47bc
Kagami <shimura.kyohei7617@gmail.com>
2023-09-02 07:40:20 +0900

    feat: navigation bar template

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,11 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      fontFamily: {
+        opensans: ['Open Sans', 'sans-serif'],
+      },
       colors: {
-        primary: {
-          100: '#ebf8ff',
-          300: '#90cdf4',
-          500: '#4299e1',
-        },
+        body: '#050505',
       },
     },
   },
